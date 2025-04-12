18fbe4891aac7f50af2ca0f75688eac6ef7bb416
Martin Korchovski <martinkorchovski@gmail.com>
2025-04-12 16:39:02 +0200

    Implement second issue

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -75,7 +75,7 @@ class TaskManager {
 
     // 3. List tasks sorted by name
     public void sortTasksByName() {
-        // TODO: Implement sorting logic
+        tasks.sort(Comparator.comparing(Task::getName));
     }
 
     // 4. Sort tasks by priority
@@ -121,6 +121,12 @@ public class SI2025Lab1Main {
 
         // MISSING: Calls to the new methods that will be implemented
 
+	System.out.println("---Tasks before sorting---");
+        manager.printTasks();
+
+        manager.sortTasksByName();
+
+        System.out.println("---Tasks after sorting---");
         manager.printTasks();
     }
 }
